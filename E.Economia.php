c45e963abf1a1ccffa14216f1e54dc1773f52687
Coguox7 <53919007+Coguox7@users.noreply.github.com>
2019-11-14 13:16:41 -0600

    Add files via upload

---
+++ E.Economia.php
@@ -0,0 +1,25 @@
+<html>
+
+    <body>
+
+
+
+<form method="POST" action="Comprobacion.php">
+¿En este tiempo tiene conocimiento del porcentage de la ecomonia de guatemala? <input type="text" name="preg1"><br>
+
+¿Usted tiene conocimiento de este acontecimiento? <input type="text" name="preg2"><br>
+
+¿Cree que la economia Guatemalteca crecera en los años por venir? <input type="text" name="preg3"><br>
+
+¿cree que es importante para guatemala incrementar en este ambito? <input type="text" name="preg4"><br>
+
+¿Te interesa la economia de Guatemala? <input type="text" name="preg5"><br>
+
+¿Crees que esto afectara a las futuras generaciones o las beneficiara? <input type="text" name="preg6"><br>
+
+¿Que espectativa tiene de la economia de guatemala? <input type="text" name="preg7"><br>
+
+<input type="submit">
+</form>
+</body>
+</html>
