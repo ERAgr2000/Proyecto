f551d81de6f0bece8ebdc83ec93586313b96659f
Coguox7 <53919007+Coguox7@users.noreply.github.com>
2019-11-14 13:43:26 -0600

    Add files via upload

---
+++ Cuestionario.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<?php echo "<div align='center'; font-size: 40px>Encuesta politica</div>"; ?>
+</head>
+
+
+<?php echo "<div align='center'>    </div>"; ?>
+
+<form method="POST" action="Comprobacion.php">
+<?php echo "<div align='center'>Usted sabia de esta noticia?\n</div>"; ?>
+<div align='center'><input type="text" name="preg1"></div>
+<?php echo "<div align='center'>Que se podria hacer ante este acontecimiento?</div>"; ?>
+<div align='center'><input type="text" name="preg2"></div>
+<?php echo "<div align='center'>Que piensa usted de este acontecimiento?</div>"; ?> 
+<div align='center'><input type="text" name="preg2"></div>
+<?php echo "<div align='center'>Esto perjudica a la sociedad ?</div>"; ?>
+<div align='center'><input type="text" name="preg2"></div>
+<?php echo "<div align='center'>  </div>"; ?>
+<div align='center'><input type="submit"></div>
+</form>
+
+</body>
+</html>
